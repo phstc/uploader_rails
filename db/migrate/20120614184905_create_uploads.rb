78aebbf46b3c5935f97c07165e5e14df0f674c7a
Pablo Cantero <pablo@pablocantero.com>
2012-06-14 19:18:08 -0300

    It seems there is a problem running rake db:drop db:migrate, if you had paperclip before and used its specific migration method has_attached_file then you remove it references.
    undefined method `has_attached_file' for #<ActiveRecord::ConnectionAdapters::TableDefinition:0x007ff9f7d4dc78>

--- db/migrate/20120614184905_create_uploads.rb
+++ db/migrate/20120614184905_create_uploads.rb
@@ -2,7 +2,10 @@ class CreateUploads < ActiveRecord::Migration
   def change
     create_table :uploads do |t|
       t.text :description
-      t.has_attached_file :file
+      t.string :file_file_name
+      t.string :file_content_type
+      t.integer :file_file_size
+      t.date :file_updated_at
       t.timestamps
     end
   end
