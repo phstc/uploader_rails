80bcc4c989c692ba52d1f0d371744fd49c72005c
Pablo Cantero <pablo@pablocantero.com>
2012-06-14 16:43:41 -0300

    Integrating with S3

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,9 @@
 class Upload < ActiveRecord::Base
-  has_attached_file :file
+  has_attached_file :file,
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
 end
