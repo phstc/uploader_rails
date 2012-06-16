fe9465ea91779c110865665db08b35d6f4ce2fbd
Pablo Cantero <pablo@pablocantero.com>
2012-06-16 17:45:22 -0300

    Trying another syntax

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -1,18 +1,11 @@
 require 'spec_helper'
 
 describe Upload do
-  describe 'Validations' do
-    # http://blog.davidchelimsky.net/2012/05/13/spec-smell-explicit-use-of-subject/
-    # it { should validates_presence_of(:description) }
-    # it { should validates_presence_of(:file_url) }
-    it "should validate presence of description" do
-      upload = Upload.new
-      upload.should have(1).error_on(:description)
-    end
-    
-    it "should validate presence of file_url" do
-      upload = Upload.new
-      upload.should have(1).error_on(:file_url)
+  describe 'Attributes validation' do
+    context "Empty attributes" do
+      let(:empty_upload) { Upload.new }   
+      it { empty_upload.should have(1).error_on(:description) }
+      it { empty_upload.should have(1).error_on(:file_url) }
     end
   end
 end
