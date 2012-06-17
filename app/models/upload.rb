f1b2cfa0402b3db4b92093840ab2dc908851e2da
Pablo Cantero <pablo@pablocantero.com>
2012-06-17 01:54:25 -0300

    Adding credentials to upload

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,10 @@
 class Upload < ActiveRecord::Base  
   validates_presence_of :description
   validates_presence_of :file_url
+  
+  delegate :timestamp, :security_hash, to: :credential, prefix: true
+  
+  def credential
+    @credential ||= UploadCredential.new
+  end
 end
--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -8,4 +8,14 @@ describe Upload do
       it { empty_upload.should have(1).error_on(:file_url) }
     end
   end
+  
+  describe 'Credential' do
+    context 'Delegation' do
+      it 'should respond_to UploadCredential attributes' do
+        upload = Upload.new
+        upload.respond_to?(:credential_security_hash).should be_true
+        upload.respond_to?(:credential_timestamp).should be_true
+      end
+    end
+  end
 end
